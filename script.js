107879784bf4f05a8c8702eaadfc5a87af941d79
vette1528 <125705405+kauevette@users.noreply.github.com>
2024-02-20 14:48:19 -0300

    envio de correção

--- script.js
+++ script.js
@@ -9,14 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
             if (audio.paused || audio.src !== audioPath) {
                 audio.src = audioPath;
                 audio.play();
-
-                // Atualiza o texto de todos os botões
-                playButtons.forEach(function (otherButton) {
-                    otherButton.innerHTML = 'Reproduzir Música';
-                });
-
-                // Atualiza o texto do botão clicado
-                this.innerHTML = 'Pausar Música';
             } else {
                 audio.pause();
                 this.innerHTML = 'Reproduzir Música';
